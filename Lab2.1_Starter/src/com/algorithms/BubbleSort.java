b065cb38797ba1bbe1ac7acacd4b74ed834bcf17
Kramclam <kennethramclam@gmail.com>
2022-08-24 11:27:52 -0400

    inital commit

--- Lab2.1_Starter/src/com/algorithms/BubbleSort.java
+++ Lab2.1_Starter/src/com/algorithms/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
     public static void main(String[] args){
        int[] input = {4, 15, 2, 6, 9, 22, 13};
        System.out.println(Arrays.toString(input));
-       sort(input);
+       bubbleSort(input);
        System.out.println(Arrays.toString(input));
     }
 
@@ -18,8 +18,22 @@ public class BubbleSort {
            if(array[j] > array[j + 1])
              swap(array, j, j + 1)
     */
-    public static void sort(int[] numbers){
+    public static void bubbleSort(int[] numbers){
        // TODO-Lab2.1: implement pseudo-code above
+        int last = numbers.length;
+        for(int k = 1; k < last; k++){
+            for(int j = 0; j < last - 1; j++){
+                if(numbers[j] > numbers[j + 1]){
+                    swap(numbers, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] numbers, int index1, int index2){
+        int temp = numbers[index1];
+        numbers[index1] = numbers[index2];
+        numbers[index2] = temp;
     }
     
 }
